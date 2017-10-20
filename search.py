78d90640d5e526ec6d167bcf1c05326de08912db
reyvateil <reyvateil@yandex.com>
2017-10-20 11:02:26 +0200

    result jsonified

--- search.py
+++ search.py
@@ -1,4 +1,5 @@
-import sqlite3
+import sqlite3, json, pprint
+from collections import defaultdict
 
 def get_composers_and_scores_by_name(name):
     conn = sqlite3.connect("scorelib.sqlite")
@@ -8,8 +9,13 @@ def get_composers_and_scores_by_name(name):
                        JOIN score_author ON score_author.composer = person.id \
                        JOIN score ON score.id = score_author.score \
                        WHERE person.name LIKE ?", (name,))
+
+    data = defaultdict(list)
     for item in res:
-        print("{} -- {}".format(item[0], item[1]))
+        data[item[0]].append(item[1])
+
+    print(json.dumps(data, ensure_ascii=False))
+
 
 if __name__=='__main__':
     get_composers_and_scores_by_name("%Johann%")
